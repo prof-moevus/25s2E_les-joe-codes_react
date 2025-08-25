9d79a54bf1115e6d2620ee2b8bb8bd822fdff807
Antoine F Moevus <2613844+antmoev@users.noreply.github.com>
2025-08-25 11:13:07 -0400

    🦝 Add lesson on iteration and props

---
+++ src/app/c04-from-table/page.js
@@ -0,0 +1,39 @@
+//Mock from DB
+import {red} from "next/dist/lib/picocolors";
+
+const products = [
+    {id:1, category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {id:2,category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {id:3,category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {id:4,category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+
+export default function Products() {
+
+    return (
+        <div className="flex flex-col justify-start items-center ma-3 pa-3 w-full">
+            <h1 className="text-3xl font-bold text-slate-100 ">Mes produits frais</h1>
+            <div id="container" className={"flex gap-2 flex-wrap w-1/2"}>
+                {
+                    products.map(p => {
+                        return ( <Product key={p.id} product={p} />)
+                    })
+                }
+
+
+            </div>
+        </div>
+
+    )
+}
+
+function Product(props) {
+    const product = props.product;
+    return (
+        <div id={"product"} className={"ring-1 p-3 w-[12rem] bg-slate-400"}>
+            <h3 className="ma-1 text-xl font-bold mb-2">{product.name}</h3>
+            <p className={product.stocked ? "text-slate-100" : "text-red-500"}>Prix {product.price}</p>
+        </div>
+    )
+}
